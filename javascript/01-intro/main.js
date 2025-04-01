86466f5da50d38ac2a4d49b03f0f429668e79bd4
calebsenm <calebsenm@gmail.com>
2025-04-01 09:30:13 -0500

    javascript

---
+++ javascript/01-intro/main.js
@@ -0,0 +1,13 @@
+document.write("Hola");
+console.log("hola");
+window.alert("Hey...");
+
+//this is a comment 
+
+/* 
+    this is a comment 
+*/
+
+document.getElementById("myH1").textContent = "Hola nuevamente";
+document.getElementById("myP").textContent = "Hola este es un texto cargado desde javascript"
+
