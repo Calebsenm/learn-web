9c253f63005584555b46436b037dabf672ea95c8
calebsenm <calebsenm@gmail.com>
2025-04-27 21:02:03 -0500

    math

---
+++ javascript/08-math/index.js
@@ -0,0 +1,62 @@
+
+let a = Math.PI 
+console.log(a);
+
+let x = 3.94;
+let y = 2;
+let z = Math.round(x);
+console.log(z);
+
+let b = 3.14;
+let c = Math.floor(b);
+console.log(c);
+
+let d = 3.124;
+let e = Math.ceil(d);
+console.log(e);
+
+let f = 3.15;
+let g = Math.trunc(f); 
+console.log(g);
+
+let h = 5; 
+let i = 2; 
+let j = Math.pow(h , i); 
+console.log(i)
+
+
+let k = 5;  
+let l = Math.sqrt(k); 
+console.log(l)
+
+let m = 10; 
+let n = Math.log(m);
+console.log(n);
+ 
+
+n = Math.sin(m);
+console.log(n);
+
+n = Math.cos(m);
+console.log(n);
+
+n = Math.tan(m);
+console.log(n);
+
+let o = -12.004
+let p = Math.abs(o);
+console.log(p);
+
+n = Math.sign(m);
+console.log(n);
+
+let x1 = 10;
+let y1 = 20; 
+let z1 =  5;
+
+let max = Math.max(x1 , y1 , z1);
+let min = Math.min(x1 , y1 , z1);
+
+console.log(max);
+console.log(min);
+
